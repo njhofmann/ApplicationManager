701813b46a87782936cd4339e7bb580d86a8192e
NateJH <inatejh@gmail.com>
2018-07-03 12:27:06 -0400

    Began work on model implementation

---
+++ src/model/ModelImpl.java
@@ -0,0 +1,26 @@
+package model;
+
+import datatransfer.AreaData;
+import datatransfer.EventData;
+
+public class ModelImpl implements ModelInterface {
+  @Override
+  public void addArea(AreaData data) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void editArea(AreaData data) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void addEvent(EventData data) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public void editEvent(EventData data) throws IllegalArgumentException {
+
+  }
+}
